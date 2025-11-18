e12fa45201e5b2b41cbc20cfca3a08d48ae6348b
@numinousv <@wvrdian333@proton.me>
2025-11-18 11:39:12 +0100

    delete fix Co-authored-by: Anna Groshkova <annagroshkova@users.noreply.github.com> Co-authored-by: Ali Jafari <alijafariaj100@gmail.com>

--- src/script.ts
+++ src/script.ts
@@ -21,7 +21,6 @@ class Repository<T extends { id: string }> {
 
     update(id: string, updated: Partial<T>): void {
         const currentItem = this.findById(id)
-        console.log(currentItem)
         const index = this.items.findIndex((item) => item.id === id)
         if (currentItem) {
             const newItem = {
@@ -34,10 +33,10 @@ class Repository<T extends { id: string }> {
     }
 
     delete(id: string | undefined): void {
-        const index = this.items.findIndex((item) => item.id === id)
-        if (index) {
-            this.items.splice(index, 0)
-        }
+        const index = this.items.findIndex(item => item.id === id)
+        if (index >= 0) {
+            this.items.splice(index, 1)
+        }    
     }
 }
 
@@ -58,44 +57,8 @@ productRepo.add({
     tags: ['black', 'elegant', 'casual']
 })
 
-productRepo.update ('464duief', {inStock: false, price: 1699})
+// productRepo.update ('464duief', {inStock: false, price: 1699})
+productRepo.delete('464duief')
 
 console.log(productRepo)
 
-
-// const repo = new Repository()
-// repo.items = repo.
-
-// type Code = 'STUDENT' | 'VIP';
-
-// function calcTotal(price: number, taxRate: number):number {
-// return price + price * taxRate;
-// }
-
-// function applyDiscount(subtotal: number, code: Code):number {
-// if (!code) return 0;
-
-// if (code === "STUDENT") return Math.round(subtotal * 0.1);
-
-// if (code === "VIP") return 50;
-
-// return 0;
-// }
-
-// function printReceipt(customer: string, price: number, taxRate: number, discountCode: Code) {
-// const subtotal = calcTotal(price, taxRate);
-// const discount = applyDiscount(subtotal, discountCode);
-// const total = subtotal - discount;
-
-// console.log(
-// "Tack " +
-// customer.toUpperCase() +
-// "! Att betala: " +
-// total.toFixed(2) +
-// " kr"
-// );
-// }
-
-// printReceipt("alex", 199, 0.25, "STUDENT");
-
-console.log('yo')
